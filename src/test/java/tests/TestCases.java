70d99bca4b84afe9c9bb676d111cd8b879dae9f8
Madhawa Ratnayake <madhawa@intervest.lk>
2021-08-02 16:28:35 +0530

    partial price matrix now working

--- src/test/java/tests/TestCases.java
+++ src/test/java/tests/TestCases.java
@@ -14,10 +14,7 @@ import utilities.Log;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
@@ -76,7 +73,7 @@ public class TestCases<result> {
     }
 
     @Test
-    public void testPostFull() {
+    public void testPostPartial() {
         int min = 1000;
         int max = 9999;
         int num = (int) (Math.random()*(max-min+1)+min);
@@ -84,11 +81,53 @@ public class TestCases<result> {
         List<Map<String, Object>> screeningList = new ArrayList<>();
         Map<String,Object> screeningMap = new HashMap<String, Object>();
         screeningMap.put("SCREENING_ID", 1);
-        screeningMap.put("SCORE", 2);
+        screeningMap.put("SCORE", 6);
         screeningMap.put("IS_WS", "Y");
         screeningMap.put("IS_AMT", "Y");
         screeningMap.put("REGION_ID", 1);
         screeningList.add(screeningMap);
+        Map<String,Object> screeningMap2 = new HashMap<String, Object>();
+        screeningMap2.put("SCREENING_ID", 12);
+        screeningMap2.put("SCORE", 6);
+        screeningMap2.put("IS_WS", "N");
+        screeningMap2.put("IS_AMT", "N");
+        screeningMap2.put("REGION_ID", 6);
+        screeningList.add(screeningMap2);
+        Map<String,Object> screeningMap3 = new HashMap<String, Object>();
+        screeningMap3.put("SCREENING_ID", 13);
+        screeningMap3.put("SCORE", 6);
+        screeningMap3.put("IS_WS", "N");
+        screeningMap3.put("IS_AMT", "N");
+        screeningMap3.put("REGION_ID", 5);
+        screeningList.add(screeningMap3);
+        Map<String,Object> screeningMap4 = new HashMap<String, Object>();
+        screeningMap4.put("SCREENING_ID", 14);
+        screeningMap4.put("SCORE", 6);
+        screeningMap4.put("IS_WS", "N");
+        screeningMap4.put("IS_AMT", "N");
+        screeningMap4.put("REGION_ID", 4);
+        screeningList.add(screeningMap4);
+        Map<String,Object> screeningMap5 = new HashMap<String, Object>();
+        screeningMap5.put("SCREENING_ID", 15);
+        screeningMap5.put("SCORE", 6);
+        screeningMap5.put("IS_WS", "N");
+        screeningMap5.put("IS_AMT", "N");
+        screeningMap5.put("REGION_ID", 3);
+        screeningList.add(screeningMap5);
+        Map<String,Object> screeningMap6 = new HashMap<String, Object>();
+        screeningMap6.put("SCREENING_ID", 16);
+        screeningMap6.put("SCORE", 6);
+        screeningMap6.put("IS_WS", "N");
+        screeningMap6.put("IS_AMT", "N");
+        screeningMap6.put("REGION_ID", 2);
+        screeningList.add(screeningMap6);
+        Map<String,Object> screeningMap7 = new HashMap<String, Object>();
+        screeningMap7.put("SCREENING_ID", 17);
+        screeningMap7.put("SCORE", 6);
+        screeningMap7.put("IS_WS", "N");
+        screeningMap7.put("IS_AMT", "N");
+        screeningMap7.put("REGION_ID", 1);
+        screeningList.add(screeningMap7);
 
         List<Map<String, Object>> propertyList = new ArrayList<>();
         Map<String,Object> propertyMap = new HashMap<String, Object>();
@@ -105,7 +144,7 @@ public class TestCases<result> {
         Map<String,Object> jsonBody = new HashMap<String,Object>();
         jsonBody.put("DOMAIN", "WAPP");
         jsonBody.put("FLOW", "CREATE_FLOW");
-        jsonBody.put("PRICE_DATE", "2020-08-18");
+        jsonBody.put("PRICE_DATE", "2021-04-11");
         jsonBody.put("CHANNEL", "OFFLINE");
         jsonBody.put("REQUESTED_FLOW", "CREATE_FLOW");
         jsonBody.put("COUNTRY_LIST", "DEFAULT");
@@ -118,9 +157,9 @@ public class TestCases<result> {
         jsonBody.put("TDE_COVER", "Y");
         jsonBody.put("GADGET_COVER", "1");
         jsonBody.put("KEY", "ACTIVITY");
-        jsonBody.put("API_TYPE", "1");
-        jsonBody.put("FACTOR_POPUP", "N");
-        jsonBody.put("CALLBACKURL", "https://redmine.interserv.co.uk?price/notify");
+//        jsonBody.put("API_TYPE", "1");
+        jsonBody.put("FACTOR_POPUP", "Y");
+//        jsonBody.put("CALLBACKURL", "https://redmine.interserv.co.uk?price/notify");
         jsonBody.put("PROPERTY", propertyList);
 
         long telemetry = given()
@@ -142,15 +181,6 @@ public class TestCases<result> {
                 .statusCode(200)
                 .log()
                 .all();
-
-        given()
-                .contentType(ContentType.JSON)
-                .body(jsonBody)
-                .when()
-                .post(environment + "/price/wapp")
-                .then()
-                .assertThat()
-                .body("status", equalTo("success"));
     }
 
 }
